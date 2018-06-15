b7b17e2f033955a432af213f1c5e47c182a8a968
Mihail Bălan <mihail.balan.holosee@gmail.com>
2018-06-15 19:31:16 +0300

    Tcp Server-Client projects

---
+++ Cs TcpClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private static TcpClient client = null;
+
+        static private string ipString; // = "192.168.1.107";
+        static private int port = 50100;
+
+        static void Main(string[] args)
+        {
+            NetworkStream stream = null;
+
+            //Console.Write("ip: ");
+            //string ipString = Console.ReadLine();
+            ipString = "127.0.0.1";
+            Console.WriteLine("  :: Clinet");
+            Console.ReadKey();
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ipString, port);
+                Console.WriteLine(" :: connected");
+                stream = client.GetStream();
+                Console.WriteLine(" :: got stream");
+
+                while (true)
+                {
+                    Console.Write(">");
+                    string line = Console.ReadLine();
+                    if (line.ToUpper() == "EXIT")
+                        break;
+
+                    int byteCound = Encoding.ASCII.GetByteCount(line);
+
+                    byte[] sendData = Encoding.ASCII.GetBytes(line);
+
+                    stream.Write(sendData, 0, sendData.Length);
+                    Console.WriteLine(" :: sent");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(String.Format("error connect: {0}", e));
+
+                return;
+            }
+
+            Console.WriteLine("done");
+
+            stream?.Close();
+            client?.Close();
+        }
+    }
+}
---
+++ Cs TcpServer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static IList<IPAddress> GetAllIpv4Addresses()
+        {
+            List<IPAddress> addresses = new List<IPAddress>();
+
+            foreach (var i in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (var ua in i.GetIPProperties().UnicastAddresses)
+                {
+                    if (ua.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        addresses.Add(ua.Address);
+                }
+            }
+
+            return addresses;
+        }
+
+        static void ServeAClient(TcpListener server)
+        {
+            server.Start();
+            Console.WriteLine(" :: started");
+
+            Console.WriteLine(" :: w8 for client");
+
+            TcpClient client = null;
+
+            try
+            {
+                client = server.AcceptTcpClient();
+                server.Stop();
+
+                Console.WriteLine(" :: got client");
+
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine(" :: got stream");
+
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                int bytesRead;
+                Console.WriteLine(" :: reading");
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string msg = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("received: '{0}'", msg);
+
+                    Console.WriteLine(" :: reading");
+                }
+            }
+            finally
+            {
+                client?.Close();
+                Console.WriteLine(" :: client done");
+
+                server?.Stop();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" :: Server :: ");
+            Console.ReadKey();
+
+            IList<IPAddress> addresses = GetAllIpv4Addresses();
+
+            foreach (IPAddress adr in addresses)
+                Console.WriteLine(String.Format("{0}", adr));
+
+            Console.WriteLine();
+
+            TcpListener server = null;
+            try
+            {
+                string ipString = "127.0.0.1";
+                int port = 50100;
+
+                IPAddress ip = IPAddress.Parse(ipString);
+                ip = IPAddress.Any;
+
+                server = new TcpListener(ip, port);
+
+                while (true)
+                {
+                    try
+                    {
+                        ServeAClient(server);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("erro client: " + e);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("error: " + e);
+            }
+        }
+    }
+}
